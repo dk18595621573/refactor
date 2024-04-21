e86c83eaf38f86aa4bfcfdfa0e0fc31aff4b77f8
duk <dl18595621573@163.com>
2024-04-21 14:18:27 +0800

    初始化项目

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * ${description}.
+ *
+ * @author duk
+ * @date 2024-04-21 下午2:16
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
